7541de91d7b36c71bab5117a5d734aa3d8809ada
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-27 17:13:13 +0800

    used if else function to determine if characters are in uppe ror lower case that transforms them into their opposite (upper to lower and vice-versa)

--- reverse_casing_of_each_character_without_using_swapcase()_function.py
+++ reverse_casing_of_each_character_without_using_swapcase()_function.py
@@ -1,7 +1,12 @@
 #Ask user to input in impproper case
-sentence=input("Input sentence:")
+sentence=input("Input sentence in improper case:")
 
 #swapcase
+if sentence.lower([]):
+    sentence= sentence.upper([])
+
+else:
+    sentence=sentence.lower([])
 
 #print
 print(sentence)
