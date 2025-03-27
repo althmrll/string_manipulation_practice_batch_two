1b921ab55af439914b2f5d269fd44711cef13ef5
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-27 15:21:21 +0800

    wrote pseudocode and the code that enables user to input and prints it

---
+++ remove_space_at_beginning_without_using_lstrip()_function.py
@@ -0,0 +1,8 @@
+#Prog 1: lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+#Ask user to input their name
+name= input("add big space and enter your name:")
+
+#Remove space before name
+#Print the formatted name
+print(name)
