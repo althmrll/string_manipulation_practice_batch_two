887e779893705b17b23d75d059fbb8eb2c0db355
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-27 15:38:39 +0800

    wrote pseudocode and wrote code for input and code that prints it

---
+++ remove_characters_in_beginning_of_string_without_using_removeprefix()_function.py
@@ -0,0 +1,9 @@
+#removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+#ask to input
+name=input("Enter name:")
+
+#remove prefix
+
+#print function
+print(name)
