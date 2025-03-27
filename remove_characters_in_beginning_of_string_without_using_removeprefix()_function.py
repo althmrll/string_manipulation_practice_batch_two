649eeb04746d0c4ac5ef5726c29aee598a865e8a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-27 15:48:09 +0800

    used replace and added a parameter input so user has a freedom to determine the parameter

--- remove_characters_in_beginning_of_string_without_using_removeprefix()_function.py
+++ remove_characters_in_beginning_of_string_without_using_removeprefix()_function.py
@@ -1,10 +1,11 @@
 #removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 #ask to input
-name=input("Enter name:")
+name=input("Enter sentence:")
+parameter= input("Input prefix you want to remove:")
 
 #remove prefix
-name=name.removeprefix("Althea")
+name=name.replace(parameter,"")
 
 #print function
 print(name)
