cbc7ff57940a27e45504e4cd9d7bbbabc179fdc9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 14:14:53 +0800

    added new variable named 'swapcase'

--- reverse_casing_of_each_character_without_using_swapcase()_function.py
+++ reverse_casing_of_each_character_without_using_swapcase()_function.py
@@ -1,13 +1,13 @@
 #Ask user to input in impproper case
 sentence=input("Input sentence in improper case:")
+swapcase=""
 
 #swapcase
 for letter in sentence:
     if letter.islower()==True:
-        sentence+= letter.upper()
+        swapcase+= letter.upper()
 
     elif letter.isupper()==True:
-        sentence+= letter.lower()
-
-#print
-print(sentence)
+        swapcase+= letter.lower()
+    
+    print(swapcase)#print
