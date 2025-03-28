31443fcc06df46ad4e85a54dbb3e63094bb044b6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 15:30:08 +0800

    Removed all attemots and typed in the initila code,, but put the value of swapcase befor ethe input. Also removed while loop since the code is already working.

--- reverse_casing_of_each_character_without_using_swapcase()_function.py
+++ reverse_casing_of_each_character_without_using_swapcase()_function.py
@@ -1,15 +1,12 @@
-while True:
-
-    sentence=input("Input sentence in improper case:")
+swapcase=""
+sentence=input("Input sentence in improper case:")
 
 #swapcase
-    for letter in sentence:
-        if letter.isupper()==True:
-            lower_case= letter.lower()
-            sentence= sentence.replace(letter, lower_case)
+for letter in sentence:
+    if letter.isupper()==True:
+        swapcase+= letter.lower()
 
-        elif letter.islower()==True:
-            upper_case= letter.upper()
-            sentence=sentence.replace(letter, upper_case)
+    elif letter.islower()==True:
+        swapcase+= letter.upper()
 
-    print(sentence)#print
+print(swapcase)#print
