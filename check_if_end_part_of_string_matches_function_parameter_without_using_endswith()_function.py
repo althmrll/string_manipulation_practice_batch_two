2bf53710aaab3c0ea4e73496d8b97b4767450bb5
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 21:59:38 +0800

    reversed sentence and parameter to make it easier for the compjter to see if they are equal

--- check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
+++ check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
@@ -1,11 +1,14 @@
 #Ask user to input sentence
 sentence= input("Input sentence:")
+
 #Ask user for parameter
 parameter=input("Input parameter:")
 
-#prints it
-if sentence.startswith(parameter)==True: #determine if sentence ends with parameter without using endswith function
-    print("The sentence DOES NOT end with '", parameter, "'")
+endswith= sentence[::-1]
+reversed_parameter=parameter[::-1]
+
+if endswith==reversed_parameter:
+    print("The sentence DOES end with", parameter)
 
 else:
-    print("The sentence DOES end with", parameter)
+    print("The sentence DOES NOT end with", parameter)
