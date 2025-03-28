1f484b0614ec3dd66d5d07e968b317f9b529fa89
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 16:33:58 +0800

    wrote the pseudocode and codes so user can input their sentence and their parameter using input function and prints it

---
+++ add_space_characters_at_end_of_string_to_satisfy_function_parameter_without_using_ljust()_function.py
@@ -0,0 +1,7 @@
+#Ask user to input sentence
+sentence=input("Input sentence:")
+#Ask user to input parameter
+parameter=input("Input parameter")
+#Add space based on parameter inputted by user
+#print it
+print(ljust)
