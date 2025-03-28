585b405c83ace5f6bf1b330d4528979ca3525ecf
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 21:38:25 +0800

    added if-else condition to check if the sentence starts given parameter using startswith() function which automatically returns a False message.

--- check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
+++ check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
@@ -3,10 +3,9 @@ sentence= input("Input sentence:")
 #Ask user for parameter
 parameter=input("Input parameter:")
 
-endswith= sentence.find(parameter)
-
 #prints it
-if sentence.find(parameter)==-1: #determine if sentence ends with parameter without using endswith function
-    print("False")
+if sentence.startswith(parameter)==True: #determine if sentence ends with parameter without using endswith function
+    print("The sentence DOES NOT end with '", parameter, "'")
+
 else:
-    print("True")
+    print("The sentence DOES end with", parameter)
