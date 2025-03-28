f94ec2ad3ed0c596260d063943434953aed71ff7
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 18:15:33 +0800

    added separator input so user can input if they want separator. In order to do the center mechanism, I decided to write code for each step. I divided the parameter into two without remainder, determined number of separators(left & right) and combined them with the sentence using addition and multiplication

--- print_string_at_center_without_using_center()_function.py
+++ print_string_at_center_without_using_center()_function.py
@@ -2,12 +2,22 @@
 sentence=input("Input sentence:")
 
 #Ask user to input parameter
-parameter=input("Input parameter")
+parameter=input("Input parameter:")
 
-#Add space based on parameter inputted by user
-space_to_be_added=parameter-len(sentence)
-space=" "*space_to_be_added
-ljust=sentence+space
+#Ask user to input separator
+separator=input("Input separator(optional, space will be added if none is typed in):")
+if separator=="":
+    separator=" "
+else:
+    continue
+
+#center based on parameter inputted by user
+left=parameter//2
+left_separator=separator*left
+left_and_sentence=left_separator+sentence
+right=parameter-len(left_and_sentence)
+right_separator=separator*right
+center=left_and_sentence+separator*right
 
 #print it
-print(ljust)
+print(center)
