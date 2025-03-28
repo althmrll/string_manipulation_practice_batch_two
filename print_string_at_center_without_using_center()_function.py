51dbaa5618bc9f7676cb0dee5facbb7e896bd292
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 18:27:22 +0800

    added condition to ensure parameter is an integer and added a middle variable to determine the middle

--- print_string_at_center_without_using_center()_function.py
+++ print_string_at_center_without_using_center()_function.py
@@ -2,17 +2,23 @@
 sentence=input("Input sentence:")
 
 #Ask user to input parameter
-parameter=input("Input parameter:")
+while True:
+    try:
+        parameter=int(input("Input parameter:"))
+        break
+        continue
+    except:
+        print("!!PARAMETER SHOULD BE A NUMBER!!")
+
 
 #Ask user to input separator
 separator=input("Input separator(optional, space will be added if none is typed in):")
 if separator=="":
     separator=" "
-else:
-    continue
 
 #center based on parameter inputted by user
-left=parameter//2
+middle=parameter-len(sentence)
+left=middle//2
 left_separator=separator*left
 left_and_sentence=left_separator+sentence
 right=parameter-len(left_and_sentence)
