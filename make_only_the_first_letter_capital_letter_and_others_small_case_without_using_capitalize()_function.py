406c4304d3ac0a4294fd9bfb0cab223660a97725
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 16:58:46 +0800

    used upper() function to capitalize the first letter and lower() function to ensure that all letters other than the First letter is uppercase

--- make_only_the_first_letter_capital_letter_and_others_small_case_without_using_capitalize()_function.py
+++ make_only_the_first_letter_capital_letter_and_others_small_case_without_using_capitalize()_function.py
@@ -1,13 +1,11 @@
-Capitalize=""
+capitalize=""
 
 #Ask user to input sentence
 sentence=input("Input sentence:")
 
 #Capitalize first letter of each word wihout using title
-sentence=sentence.split()
-
-for elements in sentence:
-    title+=elements.capitalize()+" "
+capitalize+=sentence[0].upper()
+capitalize+=sentence[1:].lower()
 
 #print it
-print (title.strip())
+print(capitalize)
