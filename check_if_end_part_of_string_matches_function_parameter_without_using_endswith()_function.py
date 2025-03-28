2e0e2ef7c2f6044fe46e65cbbfd579741c33248e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 18:51:39 +0800

    wrote pseudocode, code so user can input using input() function, and code that prints output using print() function

---
+++ check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
@@ -0,0 +1,11 @@
+#Ask user to input sentence
+sentence= input("Input sentence:")
+#Ask user for parameter
+parameter=input("Input paramater:")
+
+#determine if sentence ends with parameter without using endswith function
+#prints it
+if:
+    print("True")
+else:
+    print("False")
