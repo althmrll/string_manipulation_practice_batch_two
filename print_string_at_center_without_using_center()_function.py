ded36bc8543a7f552a48483adadbd2419f1f4c79
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 18:01:22 +0800

    decided to use the code for ljust without using ljust() function as base

--- print_string_at_center_without_using_center()_function.py
+++ print_string_at_center_without_using_center()_function.py
@@ -1,7 +1,13 @@
-center=""
 #Ask user to input sentence
 sentence=input("Input sentence:")
 
-#center it without using center function
+#Ask user to input parameter
+parameter=input("Input parameter")
+
+#Add space based on parameter inputted by user
+space_to_be_added=parameter-len(sentence)
+space=" "*space_to_be_added
+ljust=sentence+space
+
 #print it
-print(center)
+print(ljust)
