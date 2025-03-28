a59bf5c57e42e78de85943f468fc1ffacfffd1a6
Althea De Lara <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-28 22:12:12 +0800

    Added comments so non-coders can understand what each line of code does

--- check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
+++ check_if_end_part_of_string_matches_function_parameter_without_using_endswith()_function.py
@@ -4,10 +4,10 @@ sentence= input("Input sentence:")
 #Ask user for parameter
 parameter=input("Input parameter:")
 
-endswith= sentence[::-1]
-reversed_parameter=parameter[::-1]
+endswith= sentence[::-1] #reverses sentence
+reversed_parameter=parameter[::-1] #reverses parameter
 
-if endswith.startswith(reversed_parameter):
+if endswith.startswith(reversed_parameter): #determines whether the start reverse of the sentence and parameter are equal
     print("The sentence DOES end with '", parameter, "'")
 
 else:
