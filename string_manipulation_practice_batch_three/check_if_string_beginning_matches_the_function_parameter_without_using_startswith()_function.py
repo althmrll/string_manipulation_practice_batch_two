51e3e0d9a95457c8ebfa70797b7f4c8e8a547f6e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 16:14:36 +0800

    wrote pseudocode and code that enables user to input using input function and used print function to print output message

---
+++ string_manipulation_practice_batch_three/check_if_string_beginning_matches_the_function_parameter_without_using_startswith()_function.py
@@ -0,0 +1,9 @@
+#Ask user input sentence
+sentence= input("Enter sentence:")
+
+#Ask user to input parameter
+parameter= input("Enter parameter:")
+
+#Determine if sentence starts with parameter
+#print message
+print("sentence DOES start with '", parameter, "'")
