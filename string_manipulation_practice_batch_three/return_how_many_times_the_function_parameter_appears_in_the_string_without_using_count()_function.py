ae1f5991e46e346e09ad2da9b365456e565253e1
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 19:46:33 +0800

    used for loop so code will continue to check each letter and add one to the ocunter if it is equal to the parameter typed in. Also, put everything inside a while loop so an error message will flash when the user does not type in anything.

--- string_manipulation_practice_batch_three/return_how_many_times_the_function_parameter_appears_in_the_string_without_using_count()_function.py
+++ string_manipulation_practice_batch_three/return_how_many_times_the_function_parameter_appears_in_the_string_without_using_count()_function.py
@@ -1,11 +1,22 @@
 occurence_count=0
 
-#Ask user ot input sentence
-sentence=input("Enter sentence:")
-
-#Ask user ot input parameter
-parameter=input("Enter parameter:")
+while True:
+    sentence=input("Enter sentence:") #Ask user to input sentence
+    if sentence=="":
+        print("!!THERE IS NO SENTENCE TO BASE ON!!")
+    
+    else:
+        while True:
+            parameter=input("Enter parameter:") #Ask user to input parameter
 
 #count occurence of parameter in sentence
-#print count
-print(occurence_count)
+            if parameter=="":
+                print("!!A PARAMETER IS REQUIRED!!")
+            else:
+                for letter in sentence:
+                    if letter==parameter:
+                        occurence_count+=1
+                    else:
+                        continue
+            print(occurence_count)
+            break
