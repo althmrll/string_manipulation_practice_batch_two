258b5e1984b2b793f1c6d52df258a9da94ac0181
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 18:52:51 +0800

    decided to just scrap all that shenanigans and used rfind() function instead to find first location of parameter starting from the end

--- string_manipulation_practice_batch_three/return_the_first_location_of_the_function_parameter_in_the_string_starting_from_the_last_character_without_using_rindex()_function.py
+++ string_manipulation_practice_batch_three/return_the_first_location_of_the_function_parameter_in_the_string_starting_from_the_last_character_without_using_rindex()_function.py
@@ -9,13 +9,4 @@ while True:
     except:
         print("!!INVALID INPUT. SHOULD BE NUMBER!!")
 
-#Determine first occurence of parametre starting from the last
-reversed_sentence=sentence[::-1]
-reversed_parameter=parameter[::-1]
-index=reversed_sentence.index(reversed_parameter)
-sentence_indexing=len(sentence)-1
-rindex=sentence_indexing-index
-
-
-#print index
-print(rindex)
+print(sentence.rfind(parameter))
