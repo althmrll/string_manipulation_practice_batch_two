9adb0af20a9ce977345e3b97d40e57fd5efaface
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 16:46:03 +0800

    wrote pseudocode and code that enables user to input their snetence and chosn parameter using input function. Also added a variable named 'occurence_count' to count number of occurences of parameter

---
+++ string_manipulation_practice_batch_three/return_how_many_times_the_function_parameter_appears_in_the_string_without_using_count()_function.py
@@ -0,0 +1,11 @@
+occurence_count=0
+
+#Ask user ot input sentence
+sentence=input("Enter sentence:")
+
+#Ask user ot input parameter
+parameter=input("Enter parameter:")
+
+#count occurence of parameter in sentence
+#print count
+print(occurence_count)
