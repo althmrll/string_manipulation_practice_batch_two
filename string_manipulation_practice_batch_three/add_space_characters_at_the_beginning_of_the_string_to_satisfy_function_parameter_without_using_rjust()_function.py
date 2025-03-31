705dc2b5206bb6339218bd5c79f19298adeb3b29
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 17:38:11 +0800

    moved the fill input outside of the parameter and moved the parameter input inside loop so it will continue to ask for input when invalid input is typed in. Also exchanged places of sentence and space for the rjust

--- string_manipulation_practice_batch_three/add_space_characters_at_the_beginning_of_the_string_to_satisfy_function_parameter_without_using_rjust()_function.py
+++ string_manipulation_practice_batch_three/add_space_characters_at_the_beginning_of_the_string_to_satisfy_function_parameter_without_using_rjust()_function.py
@@ -1,22 +1,23 @@
 #Ask user to input sentence
 sentence= input("Enter sentence:")
+
 #Ask user ot input parameter
 while True:
-    parameter= int(input("Enter parameter:"))
     try:
-        fill=input("Input symbol/letter you want to fill the space with (Optional):") #Ask user for fill(optional)
-        if fill=="":
-            fill=" "
-        else:
-            break
-        
+        parameter= int(input("Enter parameter:"))
+        break
+    
     except:
-        ("!!INVALID INPUT. SHOULD BE A NUMBER!!")
+        print("!!INVALID INPUT. SHOULD BE A NUMBER!!")
+
+fill=input("Input symbol/letter you want to fill the space with (Optional):") #Ask user for fill(optional)
+if fill=="":
+    fill=" "
 
 character_count=len(sentence) #Determine number of characters of sentence
 num_of_filler=parameter-character_count#Subtract number of characters of sentence to parameter
 filler=num_of_filler*fill#Multipy space to the difference of character and parameter
-rjust=sentence+filler#Add sentence and space
+rjust=filler+sentence#Add sentence and filler
 
 if fill==" ":
     print("'",rjust,"'")#Print the output
