fdc6de2afa2819455551fb650fb6e119ecaf2bba
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 16:28:12 +0800

    decided to reverse the sentence instead because I got stuck at if-else staements, deciding to use code from last batch's string manipulation practice as inspiration

--- string_manipulation_practice_batch_three/check_if_string_beginning_matches_the_function_parameter_without_using_startswith()_function.py
+++ string_manipulation_practice_batch_three/check_if_string_beginning_matches_the_function_parameter_without_using_startswith()_function.py
@@ -4,8 +4,11 @@ sentence= input("Enter sentence:")
 #Ask user to input parameter
 parameter= input("Enter parameter:")
 
+reversed_sentence=sentence[::-1]
+reversed_parameter= parameter[::-1]
+
 #Determine if sentence starts with parameter
-if sentence.endswith(parameter)==False:
+if reversed_sentence.endswith(reversed_sentence)==True:
     print("sentence DOES NOT start with '", parameter, "'")
 
 else:
