307bf70711e07c42172b471d9878ada9ace733d2
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 18:02:56 +0800

    did not realize my indentation mistake so I moved the lines of code before the comment that explains what they do. Also editted the input message that asks user to input with big space after.

--- string_manipulation_practice_batch_three/remove_space_characters_at_the_end_of_string_without_using_rstrip()_function.py
+++ string_manipulation_practice_batch_three/remove_space_characters_at_the_end_of_string_without_using_rstrip()_function.py
@@ -1,13 +1,10 @@
 #Ask user to input sentence
-sentence= input("Input sentence:")
+sentence= input("Input sentence and add big spaace after:")
 
 #strip space at the end without using rstrip
-    #reverse sentence
-    reversed_sentence=sentence[::-1]
-    #lstrip sentence
-    strip=reversed_sentence.lstrip()
-    #reverse sentence again
-    rstrip=strip[::-1]
+reversed_sentence=sentence[::-1]#reverse sentence
+strip=reversed_sentence.lstrip()    #lstrip sentence
+rstrip=strip[::-1]#reverse sentence again
 
 #print output
 print(rstrip)
