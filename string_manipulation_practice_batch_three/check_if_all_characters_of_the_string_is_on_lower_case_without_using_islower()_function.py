af8068190945850f996c36ee773db9ade2f365ee
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 15:51:31 +0800

    created condition so that an output message will be printed whether it is all in lowercase or not. Also fixed condition to add a point in lower_count whenever the isupper() function returned flase. Added another ocndition too, so that symbol are also added to lower_count to give more accurate count

--- string_manipulation_practice_batch_three/check_if_all_characters_of_the_string_is_on_lower_case_without_using_islower()_function.py
+++ string_manipulation_practice_batch_three/check_if_all_characters_of_the_string_is_on_lower_case_without_using_islower()_function.py
@@ -5,9 +5,18 @@ sentence= input("Enter sentence:")
 sentence_count= sentence.count()
 #Determine if letter is in lowercase without using islower()
 for letter in sentence:
-    if letter.isupper()==True:
+    if letter.isupper()==False:
         lower_count+=1
+    
+    elif letter.isupper()==True:
+        continue
+
     else: 
+        lower_count+=1
         continue
 
 #print
+if lower_count==sentence.count():
+    print("sentence IS in lowercase")
+else:
+    print("sentence IS NOT in lowercase")
