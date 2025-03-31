f124f80124902752421feae2d157390b343a7fad
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 13:37:54 +0800

    used function endswith() to ensure sentence ends with inptted parameter. Alaso used if-statement so code will check if condition is satisfied.

--- string_manipulation_practice_batch_three/remove_characters_at_the_end_of_the_string_according_to_function_parameter_without_using_removesuffix()_function.py
+++ string_manipulation_practice_batch_three/remove_characters_at_the_end_of_the_string_according_to_function_parameter_without_using_removesuffix()_function.py
@@ -4,5 +4,11 @@ sentence= input("Input sentence:")
 #Ask user for parameter
 parameter=input("Input parameter:")
 
+if sentence.endswith(parameter)==True:
+    remove_suffix=sentence.replace(parameter,"")
+
+else:
+    print("The parameter you entered is not a suffix.")
+
 #print sentence with no suffix
 print(remove_suffix)
