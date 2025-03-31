38427fdfd9075c1bc9da40bb84e4d6e97a83d70c
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 18:30:06 +0800

    specified parameter to be an integer and put it inside while loop, so the code will continu asking until a valid input is type din. Also subtracted the index to parameter to get the rindex(? not sure if this will work)

--- string_manipulation_practice_batch_three/return_the_first_location_of_the_function_parameter_in_the_string_starting_from_the_last_character_without_using_rindex()_function.py
+++ string_manipulation_practice_batch_three/return_the_first_location_of_the_function_parameter_in_the_string_starting_from_the_last_character_without_using_rindex()_function.py
@@ -2,12 +2,19 @@
 sentence=input("Enter sentence:")
 
 #Ask user to input parameter
-parameter=input("Enter parameter:")
+while True:
+    try:
+        parameter=int(input("Enter parameter:"))
+        break
+    except:
+        print("!!INVALID INPUT. SHOULD BE NUMBER!!")
 
 #Determine first occurence of parametre starting from the last
 reversed_sentence=sentence[::-1]
 reversed_parameter=parameter[::-1]
-rindex=reversed_sentence.index(parameter)
+index=reversed_sentence.index(parameter)
+rindex=parameter-index
+
 
 #print index
 print(rindex)
