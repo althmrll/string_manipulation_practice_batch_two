837a66b009959730ac3d6dbd6dc92235493f11ee
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 13:32:04 +0800

    wrote pseudocode, code so user can input their sentence and parameter using input and the code that prints the input using print function

---
+++ string_manipulation_practice_batch_three/remove_characters_at_the_end_of_the_string_according_to_function_parameter_without_using_removesuffix()_function.py
@@ -0,0 +1,8 @@
+#Ask user to input sentence
+sentence= input("Input sentence:")
+
+#Ask user for parameter
+parameter=input("Input parameter:")
+
+#print sentence with no suffix
+print(remove_suffix)
