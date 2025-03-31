59f6701ad25303a552767dc7a0223a8c902d93b5
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-31 19:52:11 +0800

    added condition for when parameter is a single character or not. Made it so that when parameter is ore than one character, the sentence is split into substrnigs and individually checked for the parameter. Adding one to occurence count if there is.

--- string_manipulation_practice_batch_three/return_how_many_times_the_function_parameter_appears_in_the_string_without_using_count()_function.py
+++ string_manipulation_practice_batch_three/return_how_many_times_the_function_parameter_appears_in_the_string_without_using_count()_function.py
@@ -13,10 +13,21 @@ while True:
             if parameter=="":
                 print("!!A PARAMETER IS REQUIRED!!")
             else:
-                for letter in sentence:
-                    if letter==parameter:
-                        occurence_count+=1
-                    else:
-                        continue
-            print(occurence_count)
-            break
+                if len(parameter)==1:
+                    for letter in sentence:
+                        if letter==parameter:
+                            occurence_count+=1
+                        else:
+                            continue
+                    print(occurence_count)
+                    break
+
+                elif len(parameter)>1:
+                    word_list=sentence.split()
+                    for item in word_list:
+                        if item.find(parameter)==-1:
+                            continue
+                        else:
+                            occurence_count+=1
+                    print(occurence_count)
+                    break
